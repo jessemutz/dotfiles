c0ac3b105046bd9df49bad8a4741d332eee0a76e
Jesse Mutzebaugh <jesse@newvistadigital.com>
2024-03-27 20:13:27 -0600

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/VimBeGood.lua Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/VimBeGood.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/plugins/todo.lua Add .config/nvim/stylua.toml

---
+++ private_dot_config/nvim/lua/plugins/todo.lua
@@ -0,0 +1,16 @@
+return {
+  -- Highlight todo, notes, etc in comments
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    signs = true, -- show icons in the signs column
+  },
+}
+
+-- TODO: = { icon = " ", color = "info" },
+-- HACK: = { icon = " ", color = "warning" },
+-- WARN: = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+-- PERF: = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+-- NOTE: = { icon = " ", color = "hint", alt = { "INFO" } },
+-- TEST: = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+-- FIX:  = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
