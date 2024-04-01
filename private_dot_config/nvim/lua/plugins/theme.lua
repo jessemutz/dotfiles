81b81319f6856f57edfd602b6f8e901c4dcba750
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-04-01 14:41:13 -0600

    Update .config/nvim/lua/plugins/theme.lua

--- private_dot_config/nvim/lua/plugins/theme.lua
+++ private_dot_config/nvim/lua/plugins/theme.lua
@@ -6,16 +6,23 @@ return {
   },
 
   {
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    lazy = false,
+  },
+
+  {
     "folke/tokyonight.nvim",
     name = "colors_tokyonight",
     lazy = false,
   },
+
   {
     "catppuccin/nvim",
     name = "colors_catppuccin",
     lazy = false,
   },
-  -- Set colorscheme to catppuccin
+  -- Set colorscheme to rose-pine
   -- Though I do like Tokyo-night a lot.
   {
     "LazyVim/LazyVim",
