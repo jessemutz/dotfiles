ce0df0e79483f1db73187d29950767e3075b1aaa
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-04-10 20:47:50 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/plugins/personal-plugins.lua

--- private_dot_config/nvim/lua/plugins/personal-plugins.lua
+++ private_dot_config/nvim/lua/plugins/personal-plugins.lua
@@ -24,8 +24,4 @@ return {
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>" },
     },
   },
-  -- vim.keymap.set("n", "C-h", "TmuxNavigateLeft"),
-  -- vim.keymap.set("n", "C-j", "TmuxNavigateDown"),
-  -- vim.keymap.set("n", "C-k", "TmuxNavigateUp"),
-  -- vim.keymap.set("n", "C-l", "TmuxNavigateRight"),
 }
