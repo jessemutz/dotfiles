ee2b8773c08f6c24836105760e2efa7ff0e18d7e
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-04-15 19:25:03 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/keymaps.lua

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set({ "i", "v" }, "jk", "<esc>")
