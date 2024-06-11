ac1a80819d509ee736b0b6a9ae3e59f987e2f1cf
Jesse Mutzebaugh <jesse@newvistadigital.com>
2024-06-10 21:49:17 -0600

    Update .config/nvim/lua/plugins/personal-plugins.lua

--- private_dot_config/nvim/lua/plugins/personal-plugins.lua
+++ private_dot_config/nvim/lua/plugins/personal-plugins.lua
@@ -105,6 +105,27 @@ return {
     vim.cmd.highlight({ "Normal", "guibg=none ctermbg=none" })
     vim.cmd.highlight({ "NormalNC", "guibg=none ctermbg=none" })
   end,
+
+  -- Markdown Preview
+  -- For some reason this wasn't showing up in Lazy loader
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
 }
 
 -- todo-comments.nvim examples
