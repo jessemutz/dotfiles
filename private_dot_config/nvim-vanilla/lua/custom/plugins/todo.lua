7f478560c0e3f482bfdfcf86105e1a7b984cb4b1
Jesse Mutzebaugh <jesse@newvistadigital.com>
2024-06-17 22:24:12 -0600

    Add .config/nvim-vanilla/LICENSE.md Add .config/nvim-vanilla/README.md Add .config/nvim-vanilla/doc/kickstart.txt Add .config/nvim-vanilla/doc/tags Add .config/nvim-vanilla/.github/ISSUE_TEMPLATE/bug_report.md Add .config/nvim-vanilla/.github/pull_request_template.md Add .config/nvim-vanilla/.github/workflows/stylua.yml Add .config/nvim-vanilla/.gitignore Add .config/nvim-vanilla/.stylua.toml Add .config/nvim-vanilla/init.lua Add .config/nvim-vanilla/lazy-lock.json Add .config/nvim-vanilla/lua/custom/plugins/alpha.lua Add .config/nvim-vanilla/lua/custom/plugins/autocompletion.lua Add .config/nvim-vanilla/lua/custom/plugins/autoformat.lua Add .config/nvim-vanilla/lua/custom/plugins/highlight-colors.lua Add .config/nvim-vanilla/lua/custom/plugins/init.lua Add .config/nvim-vanilla/lua/custom/plugins/kickstart-and-oneoffs.lua Add .config/nvim-vanilla/lua/custom/plugins/lsp-config.lua Add .config/nvim-vanilla/lua/custom/plugins/lualine.lua Add .config/nvim-vanilla/lua/custom/plugins/markdown-preview.lua Add .config/nvim-vanilla/lua/custom/plugins/mason.lua Add .config/nvim-vanilla/lua/custom/plugins/mini.lua Add .config/nvim-vanilla/lua/custom/plugins/telescope.lua Add .config/nvim-vanilla/lua/custom/plugins/theme.lua Add .config/nvim-vanilla/lua/custom/plugins/todo.lua Add .config/nvim-vanilla/lua/custom/plugins/tresitter.lua Add .config/nvim-vanilla/lua/custom/plugins/vim-tmux.lua Add .config/nvim-vanilla/lua/custom/plugins/which-key.lua Add .config/nvim-vanilla/lua/kickstart/health.lua Add .config/nvim-vanilla/lua/kickstart/plugins/autopairs.lua Add .config/nvim-vanilla/lua/kickstart/plugins/debug.lua Add .config/nvim-vanilla/lua/kickstart/plugins/gitsigns.lua Add .config/nvim-vanilla/lua/kickstart/plugins/indent_line.lua Add .config/nvim-vanilla/lua/kickstart/plugins/lint.lua Add .config/nvim-vanilla/lua/kickstart/plugins/neo-tree.lua Add .config/nvim-vanilla/lua/vim-autocommands.lua Add .config/nvim-vanilla/lua/vim-keymaps.lua Add .config/nvim-vanilla/lua/vim-options.lua

---
+++ private_dot_config/nvim-vanilla/lua/custom/plugins/todo.lua
@@ -0,0 +1,16 @@
+return {
+  -- Highlight todo, notes, etc in comments
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = true }
+  },
+  -- Highlight todo, notes, etc in comments
+  {
+    'folke/todo-comments.nvim', 
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = true }
+  },
+}
