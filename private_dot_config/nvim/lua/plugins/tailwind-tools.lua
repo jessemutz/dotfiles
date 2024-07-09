f750ef62ea47e748e2e54fe760b5ceda6f81cd70
Jesse NVD-Tower <jesse@newvistadigital.com>
2024-07-09 14:55:24 -0600

    Add .config/nvim/lua/plugins/tailwind-tools.lua

---
+++ private_dot_config/nvim/lua/plugins/tailwind-tools.lua
@@ -0,0 +1,11 @@
+-- Tailwind Tools
+-- https://github.com/luckasRanarison/tailwind-tools.nvim?tab=readme-ov-file#installation
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    conceal = {
+      enabled = true,
+    },
+  },
+}
