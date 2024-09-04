9ddb118bb929a39e42aaf25e1453e9be34b674dd
Jesse Mutzebaugh <jesse+linux@newvistadigital.com>
2024-09-03 19:59:04 -0600

    Add list of requirements for treesitter

---
+++ private_dot_config/nvim/lua/plugins/override-nvim-treesitter.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      -- List of available modules can be found at
+      -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#available-modules
+      ensure_installed = {
+        "astro",
+        "bash",
+        "css",
+        "go",
+        "gomod",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "php",
+        "python",
+        "query",
+        "regex",
+        "scss",
+        "sql",
+        "svelte",
+        "tmux",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
