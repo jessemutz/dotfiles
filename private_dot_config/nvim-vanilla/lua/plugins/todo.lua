61bdee38bb6cfbfcde9454fd39714a8d4f7dcf93
Jesse Mutzebaugh <jesse@newvistadigital.com>
2024-10-17 13:51:26 -0600

    Remove .config/nvim-vanilla/LICENSE.md Remove .config/nvim-vanilla/README.md Remove .config/nvim-vanilla/doc/kickstart.txt Remove .config/nvim-vanilla/doc/tags Remove .config/nvim-vanilla/.github/ISSUE_TEMPLATE/bug_report.md Remove .config/nvim-vanilla/.github/pull_request_template.md Remove .config/nvim-vanilla/.github/workflows/stylua.yml Remove .config/nvim-vanilla/.gitignore Remove .config/nvim-vanilla/.stylua.toml Remove .config/nvim-vanilla/init.lua Remove .config/nvim-vanilla/lazy-lock.json Remove .config/nvim-vanilla/lua/autocommands.lua Remove .config/nvim-vanilla/lua/custom/plugins/alpha.lua Remove .config/nvim-vanilla/lua/custom/plugins/autocompletion.lua Remove .config/nvim-vanilla/lua/custom/plugins/autoformat.lua Remove .config/nvim-vanilla/lua/custom/plugins/highlight-colors.lua Remove .config/nvim-vanilla/lua/custom/plugins/init.lua Remove .config/nvim-vanilla/lua/custom/plugins/kickstart-and-oneoffs.lua Remove .config/nvim-vanilla/lua/custom/plugins/lsp-config.lua Remove .config/nvim-vanilla/lua/custom/plugins/lualine.lua Remove .config/nvim-vanilla/lua/custom/plugins/markdown-preview.lua Remove .config/nvim-vanilla/lua/custom/plugins/mason.lua Remove .config/nvim-vanilla/lua/custom/plugins/mini.lua Remove .config/nvim-vanilla/lua/custom/plugins/telescope.lua Remove .config/nvim-vanilla/lua/custom/plugins/theme.lua Remove .config/nvim-vanilla/lua/custom/plugins/todo.lua Remove .config/nvim-vanilla/lua/custom/plugins/tresitter.lua Remove .config/nvim-vanilla/lua/custom/plugins/vim-tmux.lua Remove .config/nvim-vanilla/lua/custom/plugins/which-key.lua Remove .config/nvim-vanilla/lua/keymaps.lua Remove .config/nvim-vanilla/lua/kickstart/health.lua Remove .config/nvim-vanilla/lua/kickstart/plugins/autopairs.lua Remove .config/nvim-vanilla/lua/kickstart/plugins/debug.lua Remove .config/nvim-vanilla/lua/kickstart/plugins/gitsigns.lua Remove .config/nvim-vanilla/lua/kickstart/plugins/indent_line.lua Remove .config/nvim-vanilla/lua/kickstart/plugins/lint.lua Remove .config/nvim-vanilla/lua/kickstart/plugins/neo-tree.lua Remove .config/nvim-vanilla/lua/plugins/autocompletion.lua Remove .config/nvim-vanilla/lua/plugins/autoformat.lua Remove .config/nvim-vanilla/lua/plugins/dashboard.lua Remove .config/nvim-vanilla/lua/plugins/gitsigns.lua Remove .config/nvim-vanilla/lua/plugins/lazygit.lua Remove .config/nvim-vanilla/lua/plugins/lsp-config.lua Remove .config/nvim-vanilla/lua/plugins/lualine.lua Remove .config/nvim-vanilla/lua/plugins/neotree.lua Remove .config/nvim-vanilla/lua/plugins/telescope.lua Remove .config/nvim-vanilla/lua/plugins/theme.lua Remove .config/nvim-vanilla/lua/plugins/todo.lua Remove .config/nvim-vanilla/lua/plugins/treesitter.lua Remove .config/nvim-vanilla/lua/plugins/vim-sleuth.lua Remove .config/nvim-vanilla/lua/plugins/which-key.lua Remove .config/nvim-vanilla/lua/vim-autocommands.lua Remove .config/nvim-vanilla/lua/vim-keymaps.lua Remove .config/nvim-vanilla/lua/vim-options.lua

--- private_dot_config/nvim-vanilla/lua/plugins/todo.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- Highlight todo, notes, etc in comments
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    signs = true, -- show icons in the signs column
-  },
-}
